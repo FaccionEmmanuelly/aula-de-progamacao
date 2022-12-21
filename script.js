953edaf304378c4e95558daa64a5f910ba45ae55
AndreFelC <quilombolas.da.luz@gmail.com>
2022-12-21 20:04:32 -0300

    tarefa que o pedro pediu pra fazer pra ele (roubar elas)

---
+++ script.js
@@ -0,0 +1,30 @@
+/*const bizarro = 4
+if ( bizarro>0){
+    console.log('positivo')
+} else{
+    console.log('negativo')
+}*/
+/*let bizarro = 810
+if(bizarro>0){
+    console.log('positivo')
+}else if (bizarro===0){
+    console.log('neutro')
+    
+}
+else{
+    console.log('negativo')
+}*/
+/*const a = 4
+const b = 3
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a*b)
+console.log(a%b) */
+/*const a =3
+const b = 4
+if(a>b){
+    console.log(a)
+}else if(a<b){
+    console.log(b)
+}*
