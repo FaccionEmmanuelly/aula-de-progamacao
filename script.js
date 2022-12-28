1230594e7922917ee8da7b0dfd36a9d4f1eea9f5
AndreFelC <quilombolas.da.luz@gmail.com>
2022-12-28 18:35:06 -0300

    opa,turu bom

--- script.js
+++ script.js
@@ -27,4 +27,28 @@ if(a>b){
     console.log(a)
 }else if(a<b){
     console.log(b)
-}*
+}*/
+/*const a = 4
+const b = 4
+const c = 4
+if (a>b && a>c) { 
+    console.log (a)
+}
+else if (b>c && b>a) {
+    console.log (b)
+}
+else if (c>a && c>b) {
+    console.log (c)
+}
+else {
+    console.log ('tds sao iguais')
+}*/
+const  a = 10
+const b = 90
+const c = 80
+if (10+90+80===180){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
